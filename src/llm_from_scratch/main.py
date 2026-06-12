daa63d03a3f1a4b4310f4785d0aaf530554f23fe
José Expósito <jose.exposito89@gmail.com>
2026-06-12 18:25:18 +0200

    main: Add CLI options

--- src/llm_from_scratch/main.py
+++ src/llm_from_scratch/main.py
@@ -1,2 +1,28 @@
-def main() -> None:
-    print("Hello world!")
+import argparse
+import sys
+
+def main() -> int:
+    parser = build_cli_parser()
+    args = parser.parse_args()
+
+    return 0
+
+
+def build_cli_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        prog="llm_from_scratch",
+        description="Un LLM implementado desde cero con distintas configuraciones",
+    )
+
+    parser.add_argument(
+        "--configuration",
+        help="Configuración a utilizar por el modelo",
+        default="gpt-2",
+        choices=["gpt-2"],
+    )
+
+    return parser
+
+
+if __name__ == "__main__":
+    sys.exit(main())
