1f638d111386e0522b900200344ff3d7bfb540ab
José Expósito <jose.exposito89@gmail.com>
2026-06-18 18:46:04 +0200

    transformer_block: Add transformer block
    All the components are in place, combine them in the transformer block.

---
+++ src/llm_from_scratch/transformer_block.py
@@ -0,0 +1,39 @@
+import torch.nn as nn
+
+from llm_from_scratch.config import Config
+from llm_from_scratch.feed_forward import FeedForward
+from llm_from_scratch.layer_norm import LayerNorm
+from llm_from_scratch.multi_head_attention import MultiHeadAttention
+
+
+class TransformerBlock(nn.Module):
+    def __init__(self, config: Config) -> None:
+        super().__init__()
+
+        self.att = MultiHeadAttention(
+            d_in=config.embedding_dim,
+            d_out=config.embedding_dim,
+            context_length=config.context_length,
+            num_heads=config.n_heads,
+            dropout=config.dropout_rate,
+            qkv_bias=config.query_key_value_bias,
+        )
+        self.ff = FeedForward(config.embedding_dim)
+        self.norm1 = LayerNorm(config.embedding_dim)
+        self.norm2 = LayerNorm(config.embedding_dim)
+        self.drop_shortcut = nn.Dropout(config.dropout_rate)
+
+    def forward(self, x):
+        shortcut = x
+        x = self.norm1(x)
+        x = self.att(x)
+        x = self.drop_shortcut(x)
+        x = x + shortcut
+
+        shortcut = x
+        x = self.norm2(x)
+        x = self.ff(x)
+        x = self.drop_shortcut(x)
+        x = x + shortcut
+
+        return x
